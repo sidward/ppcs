64eeb0f0893aadbef55bc121051aa616d28f71fd
Siddharth Srinivasan <ssi@mit.edu>
2022-09-12 22:26:04 -0700

    Update zenodo download link.

--- data/spiral3d_mrf/download_data.sh
+++ data/spiral3d_mrf/download_data.sh
@@ -1,13 +1,14 @@
-wget https://zenodo.org/record/6974682/files/ksp.npy
-wget https://zenodo.org/record/6974682/files/phi.npy
-wget https://zenodo.org/record/6974682/files/trj.npy
-wget https://zenodo.org/record/6974682/files/mps_000.npy
-wget https://zenodo.org/record/6974682/files/mps_001.npy
-wget https://zenodo.org/record/6974682/files/mps_002.npy
-wget https://zenodo.org/record/6974682/files/mps_003.npy
-wget https://zenodo.org/record/6974682/files/mps_004.npy
-wget https://zenodo.org/record/6974682/files/mps_005.npy
-wget https://zenodo.org/record/6974682/files/mps_006.npy
-wget https://zenodo.org/record/6974682/files/mps_007.npy
-wget https://zenodo.org/record/6974682/files/mps_008.npy
-wget https://zenodo.org/record/6974682/files/mps_009.npy
+zid=7073495
+wget https://zenodo.org/record/${zid}/files/ksp.npy
+wget https://zenodo.org/record/${zid}/files/phi.npy
+wget https://zenodo.org/record/${zid}/files/trj.npy
+wget https://zenodo.org/record/${zid}/files/mps_000.npy
+wget https://zenodo.org/record/${zid}/files/mps_001.npy
+wget https://zenodo.org/record/${zid}/files/mps_002.npy
+wget https://zenodo.org/record/${zid}/files/mps_003.npy
+wget https://zenodo.org/record/${zid}/files/mps_004.npy
+wget https://zenodo.org/record/${zid}/files/mps_005.npy
+wget https://zenodo.org/record/${zid}/files/mps_006.npy
+wget https://zenodo.org/record/${zid}/files/mps_007.npy
+wget https://zenodo.org/record/${zid}/files/mps_008.npy
+wget https://zenodo.org/record/${zid}/files/mps_009.npy
